e0e4852398cf29eb6d9b03b793a7e6abcbb57dd1
liad.raz <raz.liad@gmail.com>
2022-07-06 19:19:28 +0300

    code: impl run function. has main event loop which running the game until the user chooses to end the game. second while loop handle all the logic of the game. It switches between two players which chooses where to position their marker on board.

--- tic_tac_toe/main.py
+++ tic_tac_toe/main.py
@@ -9,16 +9,17 @@ import random
 def display_board(board_):
     print('\n')
     print('\t   |   |   ')
-    print(f'\t {board_[6]} | {board_[7]} | {board_[8]} ')
+    print(f'\t {board_[7]} | {board_[8]} | {board_[9]} ')
     print('\t   |   |   ')
     print('\t-------------')
     print('\t   |   |   ')
-    print(f'\t {board_[3]} | {board_[4]} | {board_[5]} ')
+    print(f'\t {board_[4]} | {board_[5]} | {board_[6]} ')
     print('\t   |   |   ')
     print('\t-------------')
     print('\t   |   |   ')
-    print(f'\t {board_[0]} | {board_[1]} | {board_[2]} ')
+    print(f'\t {board_[1]} | {board_[2]} | {board_[3]} ')
     print('\t   |   |   ')
+    print('\n')
 
 
 # player chose mark X or O
@@ -26,8 +27,8 @@ def player_input():
     marker = ''
 
     # Get mark from user and validate the input
-    while not ('X' == marker) or ('O' == marker):
-        marker = input('Do you want to be X ot O? ').upper()
+    while not ('X' == marker or 'O' == marker):
+        marker = input('Do you want to be X or O? ').upper()
 
     if marker == 'X':
         return ('X', 'O')
@@ -43,14 +44,14 @@ def place_marker(board_, marker_, position_):
 # win check
 def win_check(board_, mark_):
     # check wins in vertical, horizontal and in cross
-    return board_[0] == board_[1] == board_[2] == mark_ or \
-    board_[3] == board_[4] == board_[5] == mark_ or \
-    board_[6] == board_[7] == board_[8] == mark_ or \
-    board_[0] == board_[3] == board_[6] == mark_ or \
+    return board_[1] == board_[2] == board_[3] == mark_ or \
+    board_[4] == board_[5] == board_[6] == mark_ or \
+    board_[7] == board_[8] == board_[9] == mark_ or \
     board_[1] == board_[4] == board_[7] == mark_ or \
     board_[2] == board_[5] == board_[8] == mark_ or \
-    board_[6] == board_[4] == board_[2] == mark_ or \
-    board_[8] == board_[4] == board_[0] == mark_
+    board_[3] == board_[6] == board_[9] == mark_ or \
+    board_[7] == board_[5] == board_[3] == mark_ or \
+    board_[9] == board_[5] == board_[1] == mark_
 
 
 # choose who plays first
@@ -83,48 +84,88 @@ def full_board(board_):
 
 # player choice
 def player_choice(board_):
-    acceptable_range = list(range(0, 9))
-    choice = 10
+    acceptable_range = list(range(1, 10))
+    choice = 0
 
     while choice not in acceptable_range or not space_check(board_, choice):
-        choice = int(input("Choose your next position: (0-9) "))
+        choice = int(input("Choose your next position: (1-9) "))
 
     # NOTE choice is in range 1-9. Hence, the minus 1
-    return choice - 1
+    return choice
 
 
 # replay the game
 def replay():
-    return print(f"Do you want to play again? Enter Yes or No: ").lower.startwith('y')
+    return input("Do you want to play again? Enter Yes or No: ").lower().startswith('y')
 
 
 # main function - Run the game
 def run():
-    print('Welcome to Tic Tac Toe')
-    is_round = True
+    print('\t<<< Welcome to Tic Tac Toe >>>')
+    print('\t<<< ---------------------- >>>\n')
 
-    # Start running rounds
-    while is_round:
-
+    # Start running the Game
+    while True:
         # Reset The board
-        board = [' '] * 9
+        board = ['#'] + [' '] * 10
 
         # Ask the player which mark he wants to play
         player1_marker, player2_marker = player_input()
 
-        # Who will start the agem
+        # Who will start the gmae
         turn = choose_first()
-        print(f'{turn} will go first.')
+        print(f'\n{turn} will go first.')
 
         # Get input from the user to start the game
-
-        ask_ready = input('Are yor ready to play? Enter yes or no. ').lower()
+        ask_ready = input('>>> Are yor ready to play? \tEnter yes or no. ').lower()
         if ask_ready[0] == 'y':
-            is_gmae_on = True
+            is_game_on = True
         else:
-            is_gmae_on = False
+            is_game_on = False
 
         # Start the game
-        while is_gmae_on:
+        while is_game_on:
+            os.system("clear")
+
+            display_board(board)
+            position = player_choice(board)
+
+            if 'Player 1' == turn:
+                # Player's 1 turn
+                place_marker(board, player1_marker, position)
+
+                if win_check(board, player1_marker):
+                    os.system("clear")
+                    display_board(board)
+                    print("Congratulations! You Have won the game!")
+                    is_game_on = False
+
+                elif full_board(board):
+                    os.system("clear")
+                    display_board(board)
+                    print("The game is a draw! ")
+                    is_game_on = False
+
+                turn = 'Player 2'
+
+            else:
+                # Player's 2 turn
+                place_marker(board, player2_marker, position)
+
+                if win_check(board, player2_marker):
+                    display_board(board)
+                    print("Congratulations! You Have won the game!")
+                    is_game_on = False
+                elif full_board(board):
+                    display_board(board)
+                    print("The game is a draw! ")
+                    is_game_on = False
+
+                turn = 'Player 1'
+
+        # check if the user wants to play again
+        if not replay():
+            break
 
 
+run()
