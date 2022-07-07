9b92adc4d82d953850b314c23d5d4f7125fa55f9
liad.raz <raz.liad@gmail.com>
2022-07-07 17:15:10 +0300

    code: create class card. which will hold the suit, rank and int value of a card instance.

---
+++ war_card_game/classes.py
@@ -0,0 +1,41 @@
+"""
+War Card Game
+Practicing OOP in python
+"""
+
+import random
+
+# DEFINITIONS
+# LUT of the various options of available cards
+g_suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+g_ranks = (
+    'Two', 'Three', 'Four',
+    'Five', 'Six', 'Seven',
+    'Eight', 'Nine', 'Ten',
+    'Jack', 'Queen', 'King', 'Ace'
+    )
+g_values = {
+    'Two': 2, 'Three': 3, 'Four': 4,
+    'Five': 5, 'Six': 6, 'Seven': 7,
+    'Eight': 8, 'Nine': 9, 'Ten' : 10,
+    'Jack': 11, 'Queen': 12, 'King': 13, 'Ace': 14
+}
+
+
+# CARD
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = g_values[rank]
+
+    def __str__(self):
+        return f"{self.rank} of {self.suit}"
+
+
+# DECK
+
+
+
+
+# PLAYER
